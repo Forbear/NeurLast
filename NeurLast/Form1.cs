c66329d029c1897cbbd541c0c793eb55f4fe6d10
Daniil <Silnyagindan@gmail.com>
2016-12-12 00:37:28 +0300

    Initial commit

---
+++ NeurLast/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace NeurLast {
+    public partial class Form1 : Form {
+        neurFirst neurObj = new neurFirst();
+        
+        public Form1() {
+            InitializeComponent();
+        }
+        void neurAlgorithm() {
+            int count = 0;
+            int hidenLayerCount = neurObj.getHidenLayerCount();
+            int xCount = neurObj.getXCount();
+            int inLayerCount = neurObj.getInLayerCount();
+            double e = 0;
+            neurObj.readAllParams();
+            do {
+                for (int i = 0; i < hidenLayerCount; i++) {
+                    do {
+                        neurObj.weightedSum(i);
+                        neurObj.hidenYDet();
+                        neurObj.hidenShiftYDet();
+                        neurObj.exitYDet(i);
+                        if (neurObj.failDet(i)) break;
+                        neurObj.exitStepLearnDet();
+                        neurObj.exitThoughtDet(i);
+                        neurObj.exitWeightDet(i);
+                        neurObj.hidenStepLearnDet(i);
+                        neurObj.hidenThoughtDet(i);
+                        neurObj.hidenWeightDet(i);
+                        e = neurObj.getE();
+                        count++;
+                        if (label1.InvokeRequired) label1.Invoke(new MethodInvoker(delegate { label1.Text = count.ToString(); }));
+                        if (label2.InvokeRequired) label2.Invoke(new MethodInvoker(delegate { label2.Text = e.ToString(); }));
+                    } while (true);
+                }
+                if (neurObj.allFailDet()) break;
+            } while (true);
+            if (label2.InvokeRequired) label2.Invoke(new MethodInvoker(delegate { label2.Text = e.ToString(); }));
+            neurObj.writeAllWeights();
+            for(int i = hidenLayerCount; i < hidenLayerCount + inLayerCount; i++) {
+                neurObj.weightedSum(i);
+                neurObj.hidenYDet();
+                neurObj.hidenShiftYDet();
+                neurObj.exitYDet(i);
+            }
+        }
+        void createChart() {
+            
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            Thread neurAlgorithmThread = new Thread(neurAlgorithm);
+            neurAlgorithmThread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            neurObj.randomStartParams();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            double[] massX = neurObj.getXMass();
+            double[] massY = neurObj.getYMass();
+            double min = Double.MaxValue;
+            double max = Double.MinValue;
+            for (int i = 0; i < massX.Length; i++) {
+                chart1.Series["TrueValues"].Points.AddXY(i, massX[i]);
+                if (!(i > massY.Length - 1)) {
+                    if (min > massY[i]) min = massY[i];
+                    if (max < massY[i]) max = massY[i];
+                    chart1.Series["ProphetValues"].Points.AddXY(i + 5, massY[i]);
+                }
+                if (min > massX[i]) min = massX[i];
+                if (max < massX[i]) max = massX[i];                
+            }
+            chart1.ChartAreas[0].AxisY.Maximum = max + max / 1000;
+            chart1.ChartAreas[0].AxisY.Minimum = min - min / 1000;
+        }
+    }
+}
---
+++ NeurLast/neurFirst.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace NeurLast {
+    class neurFirst {
+        const int xCount = 30;
+        const int inLayerCount = 5;
+        const int hidenLayerCount = xCount * 2 / 3;
+        const double E = 0.00001;
+        public double correntE = 0;
+        double[,] hidenWeightMass = new double[inLayerCount, hidenLayerCount];
+        double[] exitWeightMass = new double[hidenLayerCount];
+        double[] hidenThoughtMass = new double[hidenLayerCount];
+        double[] xMass = new double[xCount];
+        double[] weightedSumMass = new double[hidenLayerCount];
+        double[] hidenYMass = new double[hidenLayerCount];
+        double[] hidenShiftYMass = new double[hidenLayerCount];
+        double[] exitYMass = new double[xCount - inLayerCount];
+        public double exitStepLearn = 0;
+        public double hidenStepLearn = 0;
+        double exitT = 0;
+        public double[] getXMass() {
+            return xMass;
+        }
+        public double[] getYMass() {
+            return exitYMass;
+        }
+        public double[] getExitYMass() {
+            return exitYMass;
+        }
+        public double getE() {
+            return correntE;
+        }
+        public int getInLayerCount() {
+            return inLayerCount;
+        }
+        public int getHidenLayerCount() {
+            return hidenLayerCount;
+        }
+        public int getXCount() {
+            return xCount;
+        }
+        public void readAllParams() {
+            StreamReader sr = new StreamReader("hidenWeights.txt");
+            for(int i = 0; i < inLayerCount; i++)
+                for(int j = 0; j < hidenLayerCount; j++) hidenWeightMass[i, j] = Convert.ToDouble(sr.ReadLine());
+            sr.Close();
+            sr = new StreamReader("exitWeights.txt");
+            StreamReader srT = new StreamReader("hidenTs.txt");
+            StreamReader srY = new StreamReader("exitY.txt");
+            for(int i = 0; i < hidenLayerCount; i++) {
+                exitWeightMass[i] = Convert.ToDouble(sr.ReadLine());
+                hidenThoughtMass[i] = Convert.ToDouble(srT.ReadLine());
+                exitYMass[i] = Convert.ToDouble(srY.ReadLine());
+            }
+            sr.Close();
+            srT.Close();
+            srY.Close();
+            srT = new StreamReader("exitT.txt");
+            exitT = Convert.ToDouble(srT.ReadLine());
+            srT.Close();
+            sr = new StreamReader("inParams.txt");
+            for(int i = 0; i < xCount; i++) {
+                xMass[i] = Convert.ToDouble(sr.ReadLine());
+            }
+            sr.Close();
+        }
+        public void writeAllWeights() {
+            StreamWriter sw = new StreamWriter("hidenWeights.txt");
+            for (int i = 0; i < inLayerCount; i++)
+                for (int j = 0; j < hidenLayerCount; j++) sw.WriteLine(hidenWeightMass[i, j].ToString());
+            sw.Close();
+            sw = new StreamWriter("exitWeights.txt");
+            StreamWriter swT = new StreamWriter("hidenTs.txt");
+            for (int i = 0; i < hidenLayerCount; i++) {
+                sw.WriteLine(exitWeightMass[i].ToString());
+                swT.WriteLine(hidenThoughtMass[i].ToString());
+            }
+            sw.Close();
+            swT.Close();
+            sw = new StreamWriter("exitY.txt");
+            for (int i = 0; i < exitYMass.Length; i++) sw.WriteLine(exitYMass[i].ToString());
+            sw.Close();
+            swT = new StreamWriter("exitT.txt");
+            swT.WriteLine(exitT.ToString());
+            swT.Close();
+        }
+        public void randomStartParams() {
+            Random r = new Random();
+            StreamWriter sw = new StreamWriter("hidenWeights.txt");
+            for (int i = 0; i < inLayerCount; i++) 
+                for (int j = 0; j < hidenLayerCount; j++) sw.WriteLine((r.NextDouble() / 10 - 0.05).ToString());
+            sw.Close();
+            sw = new StreamWriter("exitWeights.txt");
+            StreamWriter swT = new StreamWriter("hidenTs.txt");
+            for(int i =0; i < hidenLayerCount; i++) {
+                sw.WriteLine((r.NextDouble() / 10 - 0.05).ToString());
+                swT.WriteLine("1");
+            }
+            sw.Close();
+            swT.Close();
+            swT = new StreamWriter("exitT.txt");
+            swT.WriteLine("1");
+            swT.Close();
+        }
+        public void weightedSum(int step) {
+            double sum = 0;
+            for (int i = 0; i < hidenLayerCount; i++) {
+                for (int j = 0; j < inLayerCount; j++) sum += hidenWeightMass[j, i] * xMass[j + step];
+                weightedSumMass[i] = sum - hidenThoughtMass[i];
+            }
+        }
+        public void hidenYDet() {
+            for(int i = 0; i < hidenLayerCount; i++) hidenYMass[i] = Math.Log(weightedSumMass[i] + Math.Sqrt(weightedSumMass[i] * weightedSumMass[i] + 1));
+        }
+        public void hidenShiftYDet() {
+            for(int i = 0; i < hidenLayerCount; i++)  hidenShiftYMass[i] = 1 / (Math.Sqrt(1 + weightedSumMass[i] * weightedSumMass[i]));
+        }
+        public void exitYDet(int step) {
+            double sum = 0;
+            for (int i = 0; i < hidenLayerCount; i++) sum += hidenYMass[i] * exitWeightMass[i];
+            exitYMass[step] =  sum - exitT;
+        }
+        public void exitStepLearnDet(){
+            double sum = 0;
+            for (int i = 0; i < hidenLayerCount; i++) sum += hidenYMass[i] * hidenYMass[i];
+            exitStepLearn = 1 / (1 + sum);
+        }
+        public void exitThoughtDet(int step) {
+            exitT += exitStepLearn * (exitYMass[step] - xMass[step + inLayerCount]);
+        }
+        public void exitWeightDet(int step) {
+            for (int i = 0; i < hidenLayerCount; i++) exitWeightMass[i] -= exitStepLearn * (exitYMass[step] - xMass[step + inLayerCount]) * hidenYMass[i];
+        }
+        public void hidenStepLearnDet(int step) {
+            double sumOne = 0;
+            double sumDuo = 0;
+            double sumTre = 0;
+            for(int i = 0; i < hidenLayerCount; i++) {
+                sumOne += exitWeightMass[i] * hidenShiftYMass[i];
+                sumTre += exitWeightMass[i] * exitWeightMass[i] * hidenShiftYMass[i] * hidenShiftYMass[i];
+            }
+            for (int i = 0; i < inLayerCount; i++) sumDuo += xMass[step + i];
+            hidenStepLearn = sumOne / (sumDuo * sumTre); 
+        }
+        public void hidenThoughtDet(int step) {
+            for (int i = 0; i < hidenLayerCount; i++)
+                hidenThoughtMass[i] += hidenStepLearn * (exitYMass[step] - xMass[step + inLayerCount]) * hidenYMass[i] * exitWeightMass[i] * hidenShiftYMass[i];
+        }
+        public void hidenWeightDet(int step) {
+            for (int i = 0; i < inLayerCount; i++)
+                for (int j = 0; j < hidenLayerCount; j++)
+                    hidenWeightMass[i, j] -= hidenStepLearn * (exitYMass[step] - xMass[step + inLayerCount]) * hidenYMass[j] * exitWeightMass[j] * xMass[step + i + inLayerCount] * hidenShiftYMass[j];
+        }
+        public bool failDet(int step) {
+            double rez = 0.5 * (xMass[step + inLayerCount] - exitYMass[step]) * (xMass[step + inLayerCount] - exitYMass[step]);
+            if (rez < E) return true;
+            else return false;
+        }
+        public bool allFailDet() {
+            double sum = 0;
+            for (int i = 0; i < 20; i++) sum += (exitYMass[i] - xMass[i + inLayerCount]) * (exitYMass[i] - xMass[i + inLayerCount]);
+            correntE = sum /= 2;
+            if (sum < E) return true;
+            else return false;
+        }
+    }
+}
